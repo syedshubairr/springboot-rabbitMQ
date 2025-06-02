8597548c358498de67925d768f249ab968ed06e2
Shubair Raza <110731556+syedshubairr@users.noreply.github.com>
2025-06-02 16:08:34 +0500

    Configured Exchange, Binding and Queue in RabbitMQ.

---
+++ src/main/java/com/shubair/rabbitMq/config/RabbitMQConfig.java
@@ -0,0 +1,38 @@
+package com.shubair.rabbitMq.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    @Value("${spring.rabbitmq.queueName}")
+    private String queueName;
+    @Value("${spring.rabbitmq.exchange.name}")
+    private String exchange;
+    @Value("${spring.rabbitmq.routing.key}")
+    private String routingKey;
+
+    //  Spring bean for Rabbit MQ.
+    @Bean
+    public Queue queue() {
+        return new Queue(queueName);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(exchange);
+    }
+
+    //    Binding between Queue and exchange of RabbitMQ.
+    @Bean
+    public Binding binding() {
+        return BindingBuilder.bind(queue())
+                .to(exchange())
+                .with(routingKey);
+    }
+}
