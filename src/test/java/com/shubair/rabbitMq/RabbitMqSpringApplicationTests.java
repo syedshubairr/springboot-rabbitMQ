91b199bdfb57bc9151634432fdd23c2b47893c58
Shubair Raza <110731556+syedshubairr@users.noreply.github.com>
2025-06-02 15:20:33 +0500

    Project Initialized.

---
+++ src/test/java/com/shubair/rabbitMq/RabbitMqSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubair.rabbitMq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
